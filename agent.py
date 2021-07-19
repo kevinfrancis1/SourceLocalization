Adds methane shape and gradient. Todos for fixing q-table and reward.

agent.py
@@ -1,11 +1,14 @@
 import random
 import sys
 from environment import Environment
+from point3d import Point3d
 from utils import debug_print
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+
 REWARD = 100
+
 
 # Template for the agent containing agents x and y coordinate within the world.
 # Contains fnx initializing the location of the agent within the world
@@ -14,23 +17,20 @@ REWARD = 100
 # fnx move_: e,w,n,s provide changes to the appropriate coordinate to move N, S, E, W.  origin at bottom left
 
 
-
 class Agent:
-
     NONE = 'none'
-    POSX = (1, 0, 0)
-    NEGX = (-1, 0, 0)
-    NEGY = (0, -1, 0)
-    POSY = (0, 1, 0)
-    POSZ = (0, 0, 1)
-    NEGZ = (0, 0, -1)
+    POSX = Point3d(1, 0, 0)
+    NEGX = Point3d(-1, 0, 0)
+    NEGY = Point3d(0, -1, 0)
+    POSY = Point3d(0, 1, 0)
+    POSZ = Point3d(0, 0, 1)
+    NEGZ = Point3d(0, 0, -1)
 
     MOVES = [NEGY, POSY, POSX, NEGX, POSZ, NEGZ]
 
-    def __init__(self, x, y, z, space_width, space_height, space_depth, epsilon_decay=0.95, epsilon=1):
-        self.x = x
-        self.y = y
-        self.z = z
+    def __init__(self, location: Point3d, space_width, space_height, space_depth, epsilon_decay=0.95, epsilon=1):
+        self.location = location
+
         self.width = space_width
         self.height = space_height
         self.depth = space_depth
@@ -56,17 +56,19 @@ class Agent:
         # Make Dict
         self.experiences = []
         # Make 3D numpy array
-
-        self.utility_table = np.zeros((self.width, self.height, self.depth))
+        # utility table array of arrays methane value of d10 and methane gradient (x,y,z) of d12 and moves of d6
+        # todo add O2, O2gradient and salinity once they have been added to environment.
+        self.q_table = np.zeros((10, 12, 12, 12, 6))
 
     def distance_to(self, point):
-        manhattan_distance = abs(self.x - point[0]) + abs(self.y - point[1]) + abs(self.z - point[2])
-        return manhattan_distance
-
-    def location_string(self):
-        return '(%s, %s, %s)' % (self.x, self.y, self.z)
+        return self.location.manhattan_distance(point)
 
     def last_pollution(self):
+        if len(self.experiences) == 0:
+            return 0
+        return self.experiences[len(self.experiences) - 1][0]
+
+    def last_gradient(self):
         if len(self.experiences) == 0:
             return 0
         return self.experiences[len(self.experiences) - 1][1]
@@ -78,22 +80,22 @@ class Agent:
 
     def potential_moves(self):
         moves = []
-        if self.x > 0:
+        if self.location.x > 0:
             moves.append(Agent.NEGX)
 
-        if self.x < self.width - 1:
+        if self.location.x < self.width - 1:
             moves.append(Agent.POSX)
 
-        if self.y > 0:
+        if self.location.y > 0:
             moves.append(Agent.NEGY)
 
-        if self.y < self.height - 1:
+        if self.location.y < self.height - 1:
             moves.append(Agent.POSY)
 
-        if self.z > 0:
+        if self.location.z > 0:
             moves.append(Agent.NEGZ)
 
-        if self.z < self.depth - 1:
+        if self.location.z < self.depth - 1:
             moves.append(Agent.POSZ)
 
         return moves
@@ -101,17 +103,17 @@ class Agent:
     def choose_action(self, environment: Environment):
 
         # perceive environment status based on location
-        pollution = environment.get_pollution(self.x, self.y, self.z)
-        do2 = environment.get_do2(self.x, self.y, self.z)
+        pollution = environment.get_methane(self.location)
+        do2 = environment.get_do2(self.location)
 
         # if agent is at source it will do nothing
         if pollution == Environment.SOURCE:
-            debug_print('source found at, %s. Pollution: %s do2: %s' % (self.location_string(), pollution, do2))
+            debug_print('source found at, %s. Pollution: %s do2: %s' % (self.location.to_string(), pollution, do2))
             print('Found Source')
             return Agent.NONE
 
         # Error checking
-        if self.x < 0 or self.y < 0 or self.z < 0:
+        if self.location.x < 0 or self.location.y < 0 or self.location.z < 0:
             print('ERROR: Agent is outside of bounds, exiting')
             sys.exit()
 
@@ -127,18 +129,21 @@ class Agent:
         if random.random() < self.epsilon:
             action = random.choice(moves)
             debug_print('Moving %s from %s. Pollution: %s do2: %s'
-                        % (action, self.location_string(), pollution, do2))
+                        % (action, self.location.to_string(), pollution, do2))
             return action
-
 
         # Exploitation of Action Policy
         # exploit
         best_utility = None
         best_move = None
+        # updates action in q table
+        # todo check to make sure this is updating the action in the q-table idx(4)
         for move in moves:
-            debug_print('Checking move %s.' % (move, ))
-            x, y, z = self.next_local(move)
-            utility = self.utility_table[x, y, z]
+            debug_print('Checking move %s.' % (move.to_string()))
+            next_local = self.location.clone()
+            next_local.add(move)
+            # todo this doesn't work because it only points to the action slice, not the action paired with states
+            utility = self.q_table[4]
             if best_utility is None or utility > best_utility:
                 best_utility = utility
                 best_move = move
@@ -146,66 +151,46 @@ class Agent:
         debug_print('choose based on utility')
         return best_move
 
-        # if pollution is increasing and DO2 is decreasing the agent will return the previous action otherwise random.
-        # if do2 < self.last_do2():
-        #     debug_print('Moving %s, from %s pollution: %s do2: %s.'
-        #                 % (self.previous_action, self.location_string(), pollution, do2))
-        #     return self.previous_action
-        #
-        # if do2 == self.last_do2():
-        #     if pollution >= self.last_pollution():
-        #         debug_print('Moving %s, from %s pollution: %s do2: %s.'
-        #                     % (self.previous_action, self.location_string(), pollution, do2))
-        #         return self.previous_action
-        #     else:
-        #         action = random.choice((Agent.NEGX, Agent.POSX, Agent.NEGY, Agent.POSY, Agent.POSZ, Agent.NEGZ))
-        #         debug_print('Moving %s from %s pollution: %s , DO2: %s'
-        #                     % (action, self.location_string(), pollution, do2))
-        #         return action
-        #
-        # if do2 > self.last_do2():
-        #     action = random.choice((Agent.NEGX, Agent.POSX, Agent.NEGY, Agent.POSY, Agent.POSZ, Agent.NEGZ))
-        #     # debug_print('DO2 increasing. Moving %s' % action)
-        #     return action
-        #
-        # print("unknown status:", pollution)
+    # todo bucketize each parameter. Bucketize function added to utils file, needs to be implemented in the utility update.
+    # todo how to find the expected future reward.
 
-    def is_at(self, x, y, z):
-        # print(x, y, z)
-        return self.x == x and self.y == y and self.z == z
-
-    def update_utilities(self):
+    def update_q_states(self):
+        # todo re-write function based on bellman eq.
+    def update_q_states(self):
         exp_count = len(self.experiences)
         trial_u = [0] * exp_count
         for exp_idx in range(exp_count, 0, -1):
-            experience = self.experiences[exp_idx-1]
+            experience = self.experiences[exp_idx - 1]
             exp_reward = experience[3]
             for r_idx in range(exp_idx, 0, -1):
-                trial_u[r_idx-1] += exp_reward
+                trial_u[r_idx - 1] += exp_reward
                 exp_reward *= self.gamma
 
         # alpha is 0.5 (utility+trialu/2). adjust alpha by changing denominator
         for exp_idx in range(exp_count):
             experience = self.experiences[exp_idx]
-            x, y, z = experience[0]
-            current_utility = self.utility_table[x, y, z]
-            self.utility_table[x, y, z] = (current_utility + trial_u[exp_idx]) / 2
+            pollution = experience[0]
+            methane_gradient = experience[1]
+            current_utility = self.q_table[0, 1, 2, 3, 4]
+            self.q_table[0, 1, 2, 3, 4] = (current_utility + trial_u[exp_idx]) / 2
 
     def print_utilities(self):
         for x in range(self.width):
             for y in range(self.height):
                 for z in range(self.depth):
-                    print('{:.2f} '.format(self.utility_table[x, y, z]), end='')
+                    print('{:.2f} '.format(self.q_table[x, y, z]), end='')
             print()
 
     def reset(self):
-        # todo once total steps are normalized in the project.py reset to random positions
+        # todo once total steps are normalized in the project.py reset to random positions (this is old... maybe
+        #  don't do this)
         self.epsilon *= self.epsilon_decay
-        self.x = random.randint(0, self.width-1)
-        self.y = random.randint(0, self.height-1)
-        self.z = random.randint(0, self.depth-1)
+        self.location.x = random.randint(0, self.width - 1)
+        self.location.y = random.randint(0, self.height - 1)
+        self.location.z = random.randint(0, self.depth - 1)
         self.experiences.clear()
 
+    # todo update reward based on underlying phenomena
     # def reward(self, environment):
     #     reward = 0
     #     if environment.is_goal(self.x, self.y):
@@ -214,14 +199,15 @@ class Agent:
 
     # Pollution reward
     def reward(self, environment):
-        if environment.get_pollution(self.x, self.y, self.z) == Environment.SOURCE:
+        if environment.get_methane(self.location) == Environment.SOURCE:
             return 100
         return 0
 
     def act(self, action, environment: Environment):
         # allows agent to know action and status of prior environment
-        pollution = environment.get_pollution(self.x, self.y, self.z)
-        do2 = environment.get_do2(self.x, self.y, self.z)
+        pollution = environment.get_methane(self.location)
+
+        do2 = environment.get_do2(self.location)
 
         self.previous_action = action
 
@@ -229,69 +215,22 @@ class Agent:
         if action == Agent.NONE:
             return
 
-        # in case the action is left location equals location - 1
-        if action == Agent.NEGX:
-            self.move_NEGX()
-
-        #   in case the action is right location equals location + 1
-        if action == Agent.POSX:
-            self.move_POSX()
-
-        #   in case the action is right location equals location + 1
-        if action == Agent.POSY:
-            self.move_POSY()
-
-        #   in case the action is right location equals location + 1
-        if action == Agent.NEGY:
-            self.move_NEGY()
-
-        #   in case the action is right location equals location + 1
-        if action == Agent.POSZ:
-            self.move_POSZ()
-
-        #   in case the action is left location equals location - 1
-        if action == Agent.NEGZ:
-            self.move_NEGZ()
+        self.move(action)
 
         reward = self.reward(environment)
-        # Make experiences a dict with key being x, y, z
+        # Experiences hold methane gradient, do2 and reward
         self.experiences.append(
             (
-                (self.x, self.y, self.z),
                 pollution,
+                environment.methane_gradient(),
                 do2,
                 reward
+
             )
         )
 
-    def move(self, delta_x, delta_y, delta_z):
-        self.x += delta_x
-        self.y += delta_y
-        self.z += delta_z
-
-    def move_POSX(self):
-        self.move(1, 0, 0)
-
-    def move_NEGX(self):
-        self.move(-1, 0, 0)
-
-    def move_NEGY(self):
-        self.move(0, -1, 0)
-
-    def move_POSY(self):
-        self.move(0, 1, 0)
-
-    def move_POSZ(self):
-        self.move(0, 0, 1)
-
-    def move_NEGZ(self):
-        self.move(0, 0, -1)
-
-    def location(self):
-        return self.x, self.y, self.z
+    def move(self, delta: Point3d):
+        self.location.add(delta)
 
     def set_utility(self, location, utility):
-        self.utility_table[location] = utility
-
-    def next_local(self, move):
-        return self.x + move[0], self.y + move[1], self.z + move[2]
+        self.q_table[location] = utility

environment.py
@@ -4,33 +4,38 @@ import numpy as np
 # template for 2D environment containing (width, height) and pollution data for each cell. As well as debugging render.
 # Contains fnx set_pollution (allowing pollution level to be stored to a cell)
 # Contains fnx get_pollution (allowing pollution level to be indexed by cell)
+from point3d import Point3d
+
+
 class Environment:
     # status
     CLEAN = 0
     # POLLUTED = between 0-1
     SOURCE = 1
 
-    def __init__(self, width, height, depth, goal):
+    def __init__(self, width, height, depth, goal: Point3d):
         self.width = width
         self.height = height
         self.depth = depth
         self.goal = goal
-        self.pollution_data = np.zeros((width, height, depth))
+        self.methane_data = np.zeros((width, height, depth))
         self.dissolved_o2_data = np.zeros((width, height, depth))
 
-    def is_goal(self, x, y, z):
-        return x == self.goal[0] and y == self.goal[1] and z == self.goal[2]
+    def methane_gradient(self):
+        return np.gradient(self.methane_data)
 
-    def set_pollution(self, x, y, z, pollution_level):
-        self.pollution_data[x][y][z] = pollution_level
+    def is_goal(self, x, y, z):
+        return x == self.goal.x and y == self.goal.y and z == self.goal.z
+
+    def set_methane(self, location, pollution_level):
+        self.methane_data[location.x][location.y][location.z] = pollution_level
 
     def set_do2(self, x, y, z, do2_level):
         self.dissolved_o2_data[x][y][z] = do2_level
 
-    def get_pollution(self, x, y, z):
-        # print(x, y, z)
-        return self.pollution_data[x][y][z]
+    def get_methane(self, location):
+        return self.methane_data[location.x, location.y, location.z]
 
-    def get_do2(self, x, y, z):
-        return self.dissolved_o2_data[x][y][z]
+    def get_do2(self, location):
+        return self.dissolved_o2_data[location.x, location.y, location.z]
 

project2.py
@@ -3,67 +3,67 @@
 # coordinate grid 10x10
 # cells contain percentage 0-100 pollution
 import random
-import numpy as np
+import math
 import time
 
 from environment import Environment
 from agent import Agent
 from plot import PlotUtil
+from point3d import Point3d
 
 # scoring
 from utils import file_print, create_log
 
 performance_measure = []
 
-
 # utility_measure = []
+
+
+def get_logarithmic_methane_boundary(depth):
+    x = (-depth+12)**7
+    offset = 19
+    return - math.log(x) + offset
+
+
+def get_linear_methane_boundary(depth):
+    return (depth + 1) * 0.75
+
+
+def get_methane_for_depth(depth):
+    return -0.07*depth+1
+
+
+def get_methane(cell, source):
+    cell_in_source_reference_frame = Point3d(cell.x - source.x, cell.y - source.y, cell.z)
+
+    planar_distance_to_source = math.sqrt(cell_in_source_reference_frame.x**2 + cell_in_source_reference_frame.y**2)
+    # methane_boundary_distance = get_linear_methane_boundary(cell_in_source_reference_frame.z)
+    methane_boundary_distance = get_logarithmic_methane_boundary(cell_in_source_reference_frame.z)
+    methane_ratio = planar_distance_to_source / methane_boundary_distance
+    max_methane_at_z = get_methane_for_depth(cell.z)
+    methane = (1 - methane_ratio) * max_methane_at_z
+    # print(max(methane, 0))
+    return max(methane, 0)
+
 
 # Generates "world" setting size in 3D, location of the source and "pollution" in each cell
 # gradient/noise level of pollution smaller gradient -> more diffuse. More noise -> more stochastic
-def generate_environment(width: int, height: int, depth: int, source_x: int, source_y: int, source_z: int, grad=0.1,
-                         noise_amplitude=0.0):
-    environment = Environment(width, height, depth, (source_x, source_y, source_z))
+def generate_environment(width: int, height: int, depth: int, source: Point3d, grad=0.1, noise_amplitude=0.0):
+    environment = Environment(width, height, depth, source)
 
     # Sets the pollution diffusion from source to be no-noise or noisy
+
+    # todo: add methane (pollution), co2, temp, salinity, conductivity
+    # todo: change do2 distribution, and maybe update difference between source and methane
+    # todo: translate temperature to Kelvin
+    # todo: gas constant coefficients
     for x in range(width):
         for y in range(height):
             for z in range(depth):
-                delta_x = source_x - x
-                delta_y = source_y - y
-                delta_z = source_z - z
-                # formula for distance b/w 2 points
-                distance = np.sqrt(delta_x * delta_x + delta_y * delta_y + delta_z * delta_z)
-                # establishes difference in pollution level from cell to cell
-                distance_gradient = distance * grad
-                pollution_level = 1 - distance_gradient
-                # print(pollution_level)
-                do2_level = 0 + .60 * distance_gradient
-
-                if pollution_level < 1:
-                    # min prevents noise level from making pollution > 1. prevents multiple false sources
-                    local_noise = min(noise_amplitude, distance_gradient)
-                    noise_pollution = random.random() * local_noise
-                    final_pollution_level = max(0.0, pollution_level + noise_pollution)
-                    # print(final_pollution_level)
-                    environment.set_pollution(x, y, z, final_pollution_level)
-                    final_do2_level = min(1.0, do2_level)
-                    environment.set_do2(x, y, z, final_do2_level)
-                else:
-                    noise_pollution = 0
-                    # max prevents pollution level from generating negative values
-                    final_pollution_level = max(0.0, pollution_level + noise_pollution)
-                    environment.set_pollution(x, y, z, final_pollution_level)
-                    final_do2_level = min(1.0, do2_level)
-                    environment.set_do2(x, y, z, final_do2_level)
+                cell = Point3d(x, y, z)
+                methane = get_methane(cell, source)
+                environment.set_methane(cell, methane)
     return environment
-
-
-# Makes a decision about the action to do
-# Agent knows its location, perceives its status and can "recall" the status of its previous location and the action
-# it took prior
-# rules apply hill climbing global maximum if previous action < current state continue If previous > current turn around
-# expand memory of agent to include last 1-4 moves add a threshold data to reference2-4 nodes
-# implement sim anneal when threshold is not increased over time then random
 
 
 # computes cost of current location and provides a performance measure as a function of cost. Can be used later for
@@ -75,7 +75,7 @@ def generate_environment(width: int, height: int, depth: int, source_x: int, sou
 
 
 def compute_cost(agent, environment):
-    status = environment.get_pollution(agent.x, agent.y, agent.z)
+    status = environment.get_methane(agent.location)
     return 1 - status
 
 
@@ -96,9 +96,9 @@ def program(environment, agent, plot):
     cost = 0
     i = 0
 
-    # sets "lifetime" of agent in world render environment
-    for i in range(1000):
-        #plot.render_agent(agent, old=True, pause=0)
+    # sets "lifetime" of agent in world, render environment
+    for i in range(10000):
+        #plot.render_agent(agent, old=False, pause=0.01)
 
         # outputs data as a comma separated array
         # file_print([i, agent.last_pollution(), cost])
@@ -116,8 +116,8 @@ def program(environment, agent, plot):
         # print(score)
         # -- End Step
 
-        #plot.render_pollution_grid(environment.pollution_data)
-        #plot.render_agent(agent)
+        # plot.render_pollution_grid(environment.methane_data)
+        # plot.render_agent(agent)
     return compute_performance(cost), i
 
 
@@ -127,14 +127,15 @@ if __name__ == "__main__":
     width = 12
     height = 12
     depth = 12
+    source_location = Point3d(5, 5, 0)
+    agent_starting_location = Point3d(0, 0, 0)
 
     plot = PlotUtil(width, height, depth)
-    water = generate_environment(width, height, depth, 5, 5, 5, 1/21, 0.0)
-    bob = Agent(1, 1, 1, width, height, depth, 0.95, 1)
-
+    water = generate_environment(width, height, depth, source_location, 1/21, 0.0)
+    bob = Agent(agent_starting_location, width, height, depth, 0.95, 1)
 
     # render the pollution grid in 3D
-    #plot.render_pollution_grid(water.pollution_data)
+    # plot.render_pollution_grid(water.methane_data, True)
 
     log_name = time.strftime("%Y%m%d-%H%M%S", time.gmtime()) + '-12x12x12_5x5x5_train_stochastic'
     create_log(log_name)
@@ -143,16 +144,16 @@ if __name__ == "__main__":
     totavg = []
     totavgnorm = []
     k = 1
-    #while k < 11:
-        #print("Starting run " + str(k))
+    # while k < 11:
+    # print("Starting run " + str(k))
 
-    for run_index in range(2500):
+    for run_index in range(100):
         print("Finished run %s" % (run_index, ))
         distance_to_source = bob.distance_to(water.goal)
         epsilon = bob.epsilon
         perf, total_step = program(water, bob, plot)
-        bob.update_utilities()
-        #plot.render_utility(bob.utility_table)
+        bob.update_q_states()
+        plot.render_utility(bob.q_table)
 
         bob.reset()
         # print(run_index, total_step, distance_to_source, total_step / distance_to_source)
@@ -160,29 +161,29 @@ if __name__ == "__main__":
         performance_measure.append(perf)
         # prints to csv size, source location, distance, gradient, noise, steps and performance
         file_print([run_index, epsilon, total_step, distance_to_source, (total_step + 1) / (distance_to_source + 1)])
-        #totst.append(total_step)
-        #totstnorm.append((total_step + 1) / (distance_to_source + 1))
-        #k += 1
-        #totavgnorm.append(sum(totstnorm)/len(totstnorm))
-        #totavg.append(sum(totst)/len(totst))
-    #print("Average of average steps: " + str(sum(totavg)/len(totavg)))
-    #print("Average of averages normalized steps: " + str(sum(totavgnorm)/len(totavgnorm)))
-    #file_print(bob.utility_table)
-        # bob.print_utilities()
+        # totst.append(total_step)
+        # totstnorm.append((total_step + 1) / (distance_to_source + 1))
+        # k += 1
+        # totavgnorm.append(sum(totstnorm)/len(totstnorm))
+        # totavg.append(sum(totst)/len(totst))
+    # print("Average of average steps: " + str(sum(totavg)/len(totavg)))
+    # print("Average of averages normalized steps: " + str(sum(totavgnorm)/len(totavgnorm)))
+    # file_print(bob.utility_table)
+        bob.print_utilities()
 
     log_name = time.strftime("%Y%m%d-%H%M%S", time.gmtime()) + '-12x12x12_5x5x5_test_stochastic'
     create_log(log_name)
-    #water = generate_environment(width, height, depth, 5, 7, 6, 1/21, 0.0)
+    # water = generate_environment(width, height, depth, 5, 7, 6, 1/21, 0.0)
     bob.epsilon = 0.0
-    for run_index in range(2500):
-        #print("Now Testing Agent")
+    for run_index in range(0):
+        # print("Now Testing Agent")
         print("Finished run %s" % (run_index, ))
         distance_to_source = bob.distance_to(water.goal)
         epsilon = bob.epsilon
 
         perf, total_step = program(water, bob, plot)
-        #bob.update_utilities()
-        #plot.render_utility(bob.utility_table)
+        # bob.update_utilities()
+        # plot.render_utility(bob.utility_table)
 
         bob.reset()
         # print(run_index, total_step, distance_to_source, total_step / distance_to_source)
@@ -191,10 +192,10 @@ if __name__ == "__main__":
         # prints to csv size, source location, distance, gradient, noise, steps and performance
         file_print([run_index, epsilon, total_step, distance_to_source, (total_step + 1) / (distance_to_source + 1)])
 
-    #file_print(bob.utility_table)
+    # file_print(bob.utility_table)
         # bob.print_utilities()
 
-    #plot.render_utility(bob.utility_table, stop=True)
+    plot.render_utility(bob.q_table, stop=True)
 
     # performance_measure.append(program(0, generate_environment(10, 7), reflex_agent()));
 